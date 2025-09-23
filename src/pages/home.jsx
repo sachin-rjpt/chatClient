a1bc635b27bc3e1cfdc01a05d587b7e62d424c71
Sachin Rajput <sachinrajputmay28@gmail.com>
2025-09-23 14:45:00 +0530

    reply ui changes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import {io} from "socket.io-client";
 import axios from "axios"
-const socket=io("https://chat-5vv0.onrender.com");
+const server="https://chat-5vv0.onrender.com";
+const socket=io(server);
 import renderMessage from "../utils/renderMsg";
 import renderDate from "../utils/renderDate";
 import { format } from "date-fns";
@@ -9,21 +10,24 @@ export default function HomePage() {
     const [role,setRole]=useState(null);
     const [chat,setChat]=useState([]);
     const [msg,setMsg]=useState("");
+    const [replyingTo,setReplyingTo]=useState(null);
     const chatEndRef=useRef(null);
     const sendMessage=async(e)=>{
        e.preventDefault();
        const msgData={
          msg:msg,
          sender:role,
-         roomId:"permanentRoom"
+         roomId:"permanentRoom",
+         replyTo:replyingTo?replyingTo._id:null
        }
        socket.emit("sendMessage",msgData);
        setMsg("");
+       setReplyingTo(null);
     }
     // make api call for chats 
     useEffect(()=>{
       async function fetchMessages () {
-        const {data} = await axios.get(`https://chat-5vv0.onrender.com/msg/permanentRoom`);
+        const {data} = await axios.get(`${server}/msg/permanentRoom`);
         setChat((chat)=>[...chat,...data.allMsgs]); 
     };
     if(role){
@@ -38,7 +42,7 @@ export default function HomePage() {
     },[role]);
    useEffect(() => {
   chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
-}, [chat]);  
+}, [chat,replyingTo]);  
     if(!role){
       return(
         <>
@@ -65,14 +69,16 @@ export default function HomePage() {
                 <>
                 {showDate?<div className="flex justify-center  m-4"><p className="bg-sky-50 px-2 rounded-xl">{renderDate(msgDate)}</p></div>:null}
            <div key={i} className={`flex ${e.sender===role?"justify-end":"justify-start"}`}>
-            <div className={`flex w-1/2  ${e.sender===role?"justify-end":"justify-start"}`}>
-            <p  className="bg-gray-100 mx-4 my-2 p-2 rounded-xl ">{renderMessage(e.content)}<span className="text-[8px] ml-4 rounded-xl border-1 p-1 border-green-300 ">{format(msgDate,"hh:mm a")}</span> </p></div>
+            <div className={`flex w-[75%]  ${e.sender===role?"justify-end":"justify-start"}`}>
+             {e.replyTo? <p className="font-serif bg-green-50 rounded-2xl p-2 opacity-75 my-2"><u>Replying To :</u> {chat.find(m=>m._id===e.replyTo)?.content||"Message"}</p>:null}
+            <p onClick={()=>setReplyingTo(e)} className="bg-gray-100 mx-4 my-2 p-2 rounded-xl ">{renderMessage(e.content)}<span className="text-[8px] ml-4 rounded-xl border-1 p-1 border-green-300 ">{format(msgDate,"hh:mm a")}</span> </p></div>
             </div>
             </>
               )
         })}
             <div ref={chatEndRef}></div>
              </div>
+             {replyingTo?<p className=" flex justify-between w-[75%] bg-gray-100 mx-4 my-2 p-2 rounded-xl ">{renderMessage(replyingTo.content)}<button className="text-sm" onClick={()=>setReplyingTo(null)} >&#x2715;</button> </p>:null}
               <form  className="flex w-full  h-[10%]"onSubmit={sendMessage}>
              <div className="flex w-full  justify-evenly ">
                 <input className="w-[80%] h-[70%] rounded-xl ml-4 mt-4 pl-4 bg-white" type="text" placeholder="type here" value={msg} onChange={(e)=>setMsg(e.target.value)} />
